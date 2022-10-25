d0835353763fb3718337c2cf406917b1f2c7719b
Simon Moreau <simon@bim42.com>
2022-10-25 16:19:54 +0200

    write the webhook body in a table

---
+++ Models/Paypal.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+using Microsoft.Azure.Cosmos.Table;
+
+namespace Akauting
+{
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+    public class Amount
+    {
+        public string total { get; set; }
+        public string currency { get; set; }
+        public Details details { get; set; }
+    }
+
+    public class Details
+    {
+        public string subtotal { get; set; }
+        public string tax { get; set; }
+        public string shipping { get; set; }
+    }
+
+    public class Link
+    {
+        public string href { get; set; }
+        public string rel { get; set; }
+        public string method { get; set; }
+        public string encType { get; set; }
+    }
+
+    public class Resource
+    {
+        public Amount amount { get; set; }
+        public string id { get; set; }
+        public string parent_payment { get; set; }
+        public DateTime update_time { get; set; }
+        public DateTime create_time { get; set; }
+        public string payment_mode { get; set; }
+        public string state { get; set; }
+        public List<Link> links { get; set; }
+        public string protection_eligibility_type { get; set; }
+        public TransactionFee transaction_fee { get; set; }
+        public string protection_eligibility { get; set; }
+    }
+
+    public class ResponseHeaders
+    {
+        public string Date { get; set; }
+
+        [JsonPropertyName("Content-Length")]
+        public string ContentLength { get; set; }
+
+        [JsonPropertyName("HTTP/1.1 502 Bad Gateway")]
+        public string HTTP11502BadGateway { get; set; }
+        public string SERVER_INFO { get; set; }
+        public string Connection { get; set; }
+        public string Server { get; set; }
+    }
+
+    public class WebhookBody : TableEntity
+    {
+        public string id { get; set; }
+        public string create_time { get; set; }
+        public string resource_type { get; set; }
+        public string event_type { get; set; }
+        public string summary { get; set; }
+        public Resource resource { get; set; }
+        public string status { get; set; }
+        public List<Transmission> transmissions { get; set; }
+        public List<Link> links { get; set; }
+        public string json {get;set;}
+    }
+
+    public class TransactionFee
+    {
+        public string value { get; set; }
+        public string currency { get; set; }
+    }
+
+    public class Transmission
+    {
+        public string webhook_url { get; set; }
+        public ResponseHeaders response_headers { get; set; }
+        public string transmission_id { get; set; }
+        public string status { get; set; }
+        public DateTime timestamp { get; set; }
+    }
+
+
+}
--- Ping.cs
+++ Ping.cs
@@ -6,7 +6,8 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
+using System.Text.Json;
+using Microsoft.Azure.Cosmos.Table;
 
 namespace Akauting
 {
@@ -15,15 +16,33 @@ namespace Akauting
         [FunctionName("Ping")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            [Table("webhooks")] CloudTable webhookCloudTable,
+            [Table("webhooks")] IAsyncCollector<WebhookBody> webhookTable,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
-
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            WebhookBody body = JsonSerializer.Deserialize<WebhookBody>(requestBody);
+
+            body.PartitionKey = "webhook";
+            body.RowKey = body.id;
+            body.json = requestBody;
+
+            TableQuery<WebhookBody> rangeQuery = new TableQuery<WebhookBody>().Where(
+    TableQuery.CombineFilters(
+        TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, body.PartitionKey),
+        TableOperators.And,
+        TableQuery.GenerateFilterCondition("RowKey", QueryComparisons.Equal, body.RowKey)));
+
+            TableQuerySegment<WebhookBody> querySegment = await webhookCloudTable.ExecuteQuerySegmentedAsync(rangeQuery, null);
+
+            if (querySegment.Results.Count == 0)
+            {
+                await webhookTable.AddAsync(body);
+            }
+            
+            string name = "Ok";
 
             string responseMessage = string.IsNullOrEmpty(name)
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
